03c81d20ef464ad3641c0491d5fd1f34a2dad766
Andrewpqc <m13636038496@163.com>
2018-07-27 17:15:11 +0800

    integrate with cmdline.h

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+
 #include "minidocker.h"
+#include "cmdline.h"
 
+int main(int argc, char** argv){
+    std::cout << "container start..." << std::endl;
 
-int main(void){
-    std::cout<<"container start..."<<std::endl;
+    cmdline::parser cmd;
+    cmd.add<std::string>("host", 'h', "host name", true, "");
+    cmd.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
+    cmd.parse_check(argc, argv);
     
-    //config
-    minidocker::container_conf conf {
-        host:"minidocker",// 容器主机名
-        root_dir:"./rootfs",// 容器内文件系统挂载点
-        ip:"192.168.0.100",// 容器IP
-        bridge_name:"minidockerbr",// 网桥名,由setup.sh脚本创建，一个系统只运行一次
-        bridge_ip:"192.168.0.1"// 网桥IP
+    minidocker::container_conf conf{
+        host : "minidocker",          // 容器主机名
+        root_dir : "./rootfs",        // 容器内文件系统挂载点
+        ip : "192.168.0.100",         // 容器IP
+        bridge_name : "minidockerbr", // 网桥名,由setup.sh脚本创建，一个系统只运行一次
+        bridge_ip : "192.168.0.1"     // 网桥IP
     };
 
     minidocker::container myfirstcontainer(conf);
     myfirstcontainer.run();
-    std::cout<<"container exit..."<<std::endl;
 
+    std::cout  << cmd.get<std::string>("host") << ":"
+       << cmd.get<int>("port") << std::endl;
+    
+
+    std::cout << "container exit..." << std::endl;
 }
+
